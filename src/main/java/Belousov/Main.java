f63d4a85dac1e8f6bce5d02c711eed46a503d0b6
AntonBelousov2020 <61510278+AntonBelousov2020@users.noreply.github.com>
2020-03-09 13:13:36 +0400

    Add files via upload
    Исправления

--- src/main/java/Belousov/Main.java
+++ src/main/java/Belousov/Main.java
@@ -2,9 +2,9 @@ package Belousov;
 
 public class Main {
     public static void main(String[] args) throws ServiceExeption, ConverterExeption {
-        
+
         Service service = new Service();
-        
+
         service.service();
     }
 }
