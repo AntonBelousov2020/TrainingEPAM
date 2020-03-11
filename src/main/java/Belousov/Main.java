3264ad5b2346ce13a1c396e4711769610e3baddd
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-11 17:14:37 +0400

    Переделал структуру папок. Правильная структура папок

--- src/main/java/Belousov/Main.java
+++ src/main/java/Belousov/Main.java
@@ -1,8 +1,5 @@
 package Belousov;
 
-import lombok.Data;
-
-@Data
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException {
         Service service = new Service();
