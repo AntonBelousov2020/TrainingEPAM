c61699c366c2117041eb705948d8be41746b53ca
AntonBelousov2020 <61510278+AntonBelousov2020@users.noreply.github.com>
2020-03-11 17:03:26 +0400

    Add files via upload
    Правильная иерархия

--- src/main/java/Belousov/Main.java
+++ src/main/java/Belousov/Main.java
@@ -1,10 +1,11 @@
 package Belousov;
 
+import lombok.Data;
+
+@Data
 public class Main {
-    public static void main(String[] args) throws ServiceExeption, ConverterExeption {
-
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException {
         Service service = new Service();
-
         service.service();
     }
 }
