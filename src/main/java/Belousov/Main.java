baa8952c7d1a01629a5f069d976b030b4b159ff6
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-13 00:37:58 +0400

    Вроде, все сделал

--- src/main/java/Belousov/Main.java
+++ src/main/java/Belousov/Main.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException {
+    public static void main(String[] args) throws ServiceExeption, ConverterExeption {
         Service service = new Service();
         service.service();
     }
