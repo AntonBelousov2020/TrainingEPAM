7632c12741accb44c7dc84ab56ccb4c81169eb82
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-13 00:14:07 +0400

    Вроде, все сделал

--- src/main/java/Belousov/Main.java
+++ src/main/java/Belousov/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws ServiceExeption, ConverterExeption {
         
         Service service = new Service();
+        
         service.service();
     }
 }
