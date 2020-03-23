cad744886966f334518808735c996d0f0ba12ba4
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-23 21:09:05 +0400

    Добавил выполнение третьего задания

--- src/main/java/belousov/task1/Memory.java
+++ src/main/java/belousov/task1/Memory.java
@@ -1,15 +1,23 @@
 package belousov.task1;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class Memory {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        for (int i = 0; ; i++) {
-            list.add("Cat");
-            System.out.println(i);
+        try {
+            for (int i = 0; ; i++) {
+                list.add("Cat");
+                System.out.println(i);
+            }
+        } catch (OutOfMemoryError e) {
+            log.error("Ошибка " + e.toString());
+            System.exit(-1);
         }
     }
 }
--- src/main/java/belousov/task2/Stack.java
+++ src/main/java/belousov/task2/Stack.java
@@ -1,8 +1,16 @@
 package belousov.task2;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Stack {
 
-    public void stackOwerFlow() {
-        stackOwerFlow();
+    public void stackOwerFlow() throws StackOverflowError {
+        try {
+            stackOwerFlow();
+        } catch (StackOverflowError e) {
+            log.error("Ошибка " + e.toString());
+            System.exit(-1);
+        }
     }
 }
