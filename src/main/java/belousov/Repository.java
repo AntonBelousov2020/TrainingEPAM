c9c7b4999bfa3bec60db63b8c91bc4506d9c46fb
AntonBelousov2020 <61510278+AntonBelousov2020@users.noreply.github.com>
2020-03-24 12:34:22 +0400

    Add files via upload
    Изменение

---
+++ src/main/java/belousov/Repository.java
@@ -0,0 +1,8 @@
+package belousov;
+
+public  interface Repository<T,V> {
+    public V getEntity(int index);
+    public V[] getAllEntity();
+    public void setEntity(V entyty);
+    public void setAllEntity(V[] entytys);
+}
