22a1d28ec3158ba9854a33755b8ca2b077f37c3a
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-24 12:40:45 +0400

    Первый коммит

--- src/main/java/belousov/GenericWork.java
+++ src/main/java/belousov/GenericWork.java
@@ -8,6 +8,7 @@ public class GenericWork {
         public static void main(String[] args)throws ClassNotFoundException, IllegalAccessException{
             Service service = new Service();
             service.service();
+            
         }
 
 }
