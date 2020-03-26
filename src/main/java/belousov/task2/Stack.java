6de65dbc861c4653b07ef4896aa83c82b5f9d382
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-26 23:31:49 +0400

    Добавил javadoc

--- src/main/java/belousov/task2/Stack.java
+++ src/main/java/belousov/task2/Stack.java
@@ -5,6 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Stack {
 
+    /**
+     * This method try to get StackOverflowError with recursive
+     *
+     * @throws StackOverflowError in case of stack be overflow
+     */
     public void stackOwerFlow() throws StackOverflowError {
         try {
             stackOwerFlow();
--- src/main/java/belousov/task3/MyClassLoaderWork.java
+++ src/main/java/belousov/task3/MyClassLoaderWork.java
@@ -5,7 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MyClassLoaderWork {
 
-    public void myClassLoaderWork(){
+    /**
+     * This method realise main logic. In this method calling methods load for compile class. Byte code located an other
+     * directory compared with project directory
+     */
+    public void myClassLoaderWork() {
         MyClassLoader classLoader = new MyClassLoader("C:\\Users\\Anton\\Desktop\\myClasses\\");
         Class<?> clazz;
         try {
