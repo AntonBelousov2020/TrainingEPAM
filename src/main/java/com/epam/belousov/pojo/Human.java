eae4e9d2c8993616933a00d9d86fc138cb95335f
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-03-31 12:43:35 +0400

    Первый коммит

---
+++ src/main/java/com/epam/belousov/pojo/Human.java
@@ -0,0 +1,25 @@
+package com.epam.belousov.pojo;
+
+/**
+ * This pojo class. This class describe entity
+ */
+public class Human {
+    private int age;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
