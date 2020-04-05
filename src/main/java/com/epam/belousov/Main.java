2a88ab40ea3cc12a273140afceaf5af429e52086
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-04-05 11:39:37 +0400

    Сделал задание со стримами

---
+++ src/main/java/com/epam/belousov/Main.java
@@ -0,0 +1,10 @@
+package com.epam.belousov;
+
+import com.epam.belousov.task1.MyStream;
+
+public class Main {
+    public static void main(String[] args) {
+        MyStream myStream = new MyStream();
+        myStream.streamWork();
+    }
+}
---
+++ src/main/java/com/epam/belousov/task1/MyStream.java
@@ -0,0 +1,89 @@
+package com.epam.belousov.task1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * This class describe working with Stream class. This class also describe finding last day of the World
+ */
+public class MyStream {
+    private static final Logger log = LoggerFactory.getLogger(MyStream.class);
+    private static final String fileName = "UUID.txt";
+
+    /**
+     * This method realize main logic. It call other method in this class. This method is
+     * entry point for static method on this class
+     */
+    public void streamWork() {
+        Optional<List<UUID>> list = generateRandomUuid();
+        writeToFileUuid(list);
+        calcUuid();
+    }
+
+    /**
+     * This method generate random UUIDs
+     *
+     * @return list of UUIDs
+     */
+    private static Optional<List<UUID>> generateRandomUuid() {
+        Stream<UUID> stream = Stream.generate(UUID::randomUUID)
+                .limit(10000);
+        List<UUID> uuidList = stream.collect(Collectors.toList());
+//        for (int i = 0; i < uuidList.size(); i++) {
+//            log.info("UUID {}: {}", i, uuidList.get(i));
+//        }
+        return Optional.ofNullable(uuidList);
+    }
+
+    /**
+     * This method write to file generated UUIDs
+     *
+     * @param uuidList entry list included UUIDs
+     */
+    private static void writeToFileUuid(Optional<List<UUID>> uuidList) {
+        if (uuidList.isPresent()) {
+            try (Writer writer = new BufferedWriter(
+                    new FileWriter(fileName))) {
+                writer.write(String.valueOf(uuidList)
+                        .replace("Optional", "Optional\n")
+                        .replace("[", "")
+                        .replace(", ", "\n")
+                        .replace("]", ""));
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        }
+    }
+
+    /**
+     * This method read UUIDs from file and calc UUIDs with the sum of digits more than 100
+     */
+    private static void calcUuid() {
+        Path path = Paths.get(MyStream.fileName);
+        try {
+            long summary;
+            summary = Files.readAllLines(path)
+                    .stream()
+                    .map(s -> s.replace("Optional", ""))
+                    .map(s -> s.replaceAll("\\D", ""))
+                    .map(s -> s.chars().map(Character::getNumericValue).sum())
+                    .filter(summ -> summ > 100)
+                    .count();
+            log.info("The number of elements the sum of which is 100: {}" ,summary);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public static void findLastDayOfTheWorld() {}
+}
