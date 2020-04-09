a4b23f7db6ab85b92c75022706d03bbfc8a06dfd
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-04-09 23:05:07 +0400

    Сделал решение race condition и сделал deadlock

--- src/main/java/com/epam/belousov/task2/raceconditionsolution/RaceCondition.java
+++ src/main/java/com/epam/belousov/task2/raceconditionsolution/RaceCondition.java
@@ -1,19 +1,28 @@
 package com.epam.belousov.task2.raceconditionsolution;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This class describe logic for solution race condition
  */
 public class RaceCondition extends Calc {
+    private static final Logger log = LoggerFactory.getLogger(RaceCondition.class);
 
     /**
      * This method create some threads for demonstration race condition solution
      */
     public void workRaceConditionSolution() {
         synchronized (this) {
-            Thread thread1 = new Thread(this::raceCondition);
-            Thread thread2 = new Thread(this::lowCondition);
-            thread1.start();
-            thread2.start();
+            try {
+                Thread thread1 = new Thread(this::raceCondition);
+                Thread thread2 = new Thread(this::lowCondition);
+                thread1.start();
+                thread2.sleep(1000);
+                thread2.start();
+            } catch (InterruptedException e) {
+                log.error(e.getMessage());
+            }
         }
     }
 
