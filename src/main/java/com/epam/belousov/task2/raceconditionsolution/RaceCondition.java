d08485ddf6e2e81bfe6077cf6166771224cabe91
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-04-10 17:50:57 +0400

    Сделал первые два задания

--- src/main/java/com/epam/belousov/task2/raceconditionsolution/RaceCondition.java
+++ src/main/java/com/epam/belousov/task2/raceconditionsolution/RaceCondition.java
@@ -13,26 +13,21 @@ class RaceCondition extends Calc {
      * This method create some threads for demonstration race condition solution
      */
     public void workRaceConditionSolution() {
-        synchronized (this) {
-            try {
                 Thread thread1 = new Thread(this::raceCondition);
                 Thread thread2 = new Thread(this::lowCondition);
                 thread1.start();
-                thread2.sleep(1000);
                 thread2.start();
-            } catch (InterruptedException e) {
-                log.error(e.getMessage());
-            }
-        }
     }
 
     /**
      * This method make a increment for shared variable and print value this variable in loop
      */
     private void raceCondition() {
-        for (int i = 0; i < 10; i++) {
-            inc();
-            print();
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                inc();
+                print();
+            }
         }
     }
 
@@ -40,9 +35,11 @@ class RaceCondition extends Calc {
      * This method make a decrement for shared variable and print value this variable in loop
      */
     private void lowCondition() {
-        for (int i = 0; i < 10; i++) {
-            dec();
-            print();
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                dec();
+                print();
+            }
         }
     }
 }
